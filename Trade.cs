6639741eb92e7b539948c3aa4c7906942cd6a4a9
Jula Andadora <241544@student.pwr.edu.pl>
2020-04-19 17:06:14 +0200

    Dodaj pliki projektów.

---
+++ Trade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JulaFintech
+{
+    class Trade
+    {
+        public double amount { get; set; }
+        public long date { get; set; }
+        public double price { get; set; }
+        public long tid { get; set; }
+        public string type { get; set; }
+
+        public override string ToString() 
+        {
+            return $"TradeID: {tid}, Amount: {amount}, Date: {date}, Price: {price}, Type: {type}";
+        }
+    }
+}
---
+++ TradeSet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace JulaFintech
+{
+    class TradeSet
+    {
+        public IEnumerable<Trade> trades { get; }
+        public DateTime since { get; }
+        public DateTime to { get; }
+
+        public TradeSet(DateTime since, DateTime to)
+        {
+            this.since = since;
+            this.to = to;
+            this.trades = GetTradesEnum(since, to);
+        }
+
+        static IEnumerable<Trade> GetTradesEnum(DateTime since, DateTime to)
+        {
+            string data_directory = @"D:\studia\NETiJava\Fintech\DATA\BitBay";
+            var since_unix = (long)(since.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var to_unix = (long)(to.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            var since_int = int.Parse(since.ToString("yyyyMMdd"));
+            var to_int = int.Parse(to.ToString("yyyyMMdd"));
+
+            var paths = Directory.EnumerateFiles(data_directory, "BTCPLN*.json");
+            foreach (string path in paths)
+            {
+                int start = int.Parse(path.Split('_')[1]);
+                int stop = int.Parse(path.Split('_')[2]);
+                if (stop >= since_int && start <= to_int)
+                {
+                    string jsonString = File.ReadAllText(path);
+                    var split_jsonString = jsonString.Split("},{");
+                    split_jsonString[0] = split_jsonString[0].TrimStart('[');
+                    split_jsonString[0] = split_jsonString[0].TrimStart('{');
+                    split_jsonString[split_jsonString.Length - 1] = split_jsonString[0].TrimStart(']');
+                    split_jsonString[split_jsonString.Length - 1] = split_jsonString[0].TrimStart('}');
+                    foreach (string singleJson in split_jsonString)
+                    {
+                        string Json = $"{{{singleJson}}}";
+                        Trade trade = JsonSerializer.Deserialize<Trade>(Json);
+                        if (since_unix <= trade.date && trade.date <= to_unix)
+                        {
+                            yield return trade;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
